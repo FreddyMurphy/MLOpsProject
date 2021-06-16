9223d3de93d5a401d59fce406342a8acd18815a4
Patrick Nielsen <palleoel@gmail.com>
2021-06-16 15:27:27 +0200

    Updated the run_config and changed the trainer call in train_model

--- run_config.py
+++ run_config.py
@@ -1,20 +1,24 @@
 import azureml.core
-from azureml.core import Environment, Experiment, ScriptRunConfig, Workspace
+from azureml.core import Workspace, Experiment, Environment, ScriptRunConfig
 from azureml.core.conda_dependencies import CondaDependencies
-
 print("Using azureml-core version", azureml.core.VERSION)
 
 # Get workspace from local config file (download through azure portal)
 ws = Workspace.from_config()
 
 # Create environment
-env = Environment.from_pip_requirements(name="myenv", file_path='requirements.txt')
+env = Environment(workspace=ws, name="pytorch_env")
 
 # Set dependencies of env (could also have used a dependencies.txt file instead)
 packages = CondaDependencies.create(conda_packages=['pip', 'pytorch','python==3.7', 'joblib'],
                                     pip_packages=['azureml-defaults', 'torchvision==0.7.0',
-                                                  'pytorch_lightning', 'wandb'])
+                                                  'pytorch_lightning', 'wandb', 'kornia', 'torch_enhance'])
+
 env.python.conda_dependencies = packages
+# env.python.conda_dependencies.add_conda_package('pip')
+# env.python.conda_dependencies.add_conda_package('pytorch')
+# env.python.conda_dependencies.add_conda_package('python==3.7')
+# env.python.conda_dependencies.add_conda_package('joblib')
 
 # I have created both a CPU and GPU compute targets
 compute_targets = ws.compute_targets
@@ -22,8 +26,8 @@ compute_targets = ws.compute_targets
 # Run script using the GPU target and env
 config = ScriptRunConfig(
     compute_target = compute_targets['GPU'],
-    source_directory = 'src',
-    script = 'models/train_model.py',
+    source_directory = '.',
+    script = 'src/models/train_model.py',
     environment = env
 )
 
@@ -31,3 +35,4 @@ config = ScriptRunConfig(
 experiment_name = "Train_SRCNN"
 exp = Experiment(ws, experiment_name)
 run = exp.submit(config)
+run.wait_for_completion(show_output=True)
