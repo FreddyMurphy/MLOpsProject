a6fd20171ac1c46072b8bbadc9bd2b2dd13b284f
Frederik Desktop <s164537@student.dtu.dk>
2021-06-16 14:48:14 +0200

    Test data runs now

--- src/tests/test_data.py
+++ src/tests/test_data.py
@@ -1,4 +1,6 @@
 import torch
+from src.data.dataloader import DIV2K
+from torch.utils.data import DataLoader
 
 div2k_train = DIV2K('data/raw/DIV2K_train_HR')
 div2k_test = DIV2K('data/raw/DIV2K_valid_HR')
@@ -15,19 +17,13 @@ class TestData:
 
     # Test for checking that each index contains a low and high res image in train
     def test_shape_of_images_train(self):
-        for index, images in div2k_train:
+        dataloader = DataLoader(div2k_train, batch_size=1, num_workers=4)
+        for index, images in enumerate(dataloader):
             assert len(images) == 2
 
     # Test for checking that each index contains a low and high res image in test
     def test_shape_of_images(self):
-        for index, images in div2k_test:
-             assert len(images) == 2
+        dataloader = DataLoader(div2k_test, batch_size=1, num_workers=4)
+        for index, images in enumerate(dataloader):
+            assert len(images) == 2
 
-    # Test that each label is reresented once
-    def test_occurance_of_labels_train(self):
-        label_set = set()
-        test_labels = list(range(0, 10))
-        for images, labels in train_dataset:
-            label_set.add(labels.item())
-
-        assert test_labels == list(label_set)
