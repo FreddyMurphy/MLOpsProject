910a6f7195e80c912b066bd4c3cadbec9df84fe9
Rasmus Borstroem <s174298@student.dtu.dk>
2021-06-16 13:53:16 +0200

    wandb deployed

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,7 +1,10 @@
 from src.models.model import SRCNN
 from src.data.dataloader import DIV2KDataModule
 from pytorch_lightning import Trainer
-
+import wandb
+from pytorch_lightning.loggers import WandbLogger
+import os
+import shutil
 
 '''
 def train(model, device, trainloader, epochs=5, print_every=500):
@@ -36,21 +39,39 @@ def test(trainer, div2k, model):
     trainer.test(model=model, datamodule=div2k)
 
 if __name__ == "__main__":
+    
+    # Read api key from file
+    # File is in gitignore to ensure that key isn't pushed
+    use_wandb = True
+    try:        
+        with open("wandb_api_key.txt", encoding = 'utf-8') as f:
+            key = f.read()
+    except:
+        print("wandb_api_key.txt file not found containing api key, cannot use WandB...")
+        use_wandb = False
+    
+    logger = None
+      
+    if (use_wandb):  
+        wandb.login(key=key)
+        wandb.init(entity='MLOps14', project="DIV2K")
+        logger = WandbLogger()
+    
+    wandb.log({"Test2": 123})
+    
+    #### ACTUAL TRAINING AND TESTING
     div2k = DIV2KDataModule()
     model = SRCNN()
 
-    logger = None # Make into wandb at some point
-    '''
-    wandb.login(key='API KEY HERE')
-    wandb.init(project="MNIST")
-    
-    logger = WandbLogger()
-    '''
-    trainer = Trainer(max_epochs=100, logger=logger, gpus=1)
+    trainer = Trainer(max_epochs=100, logger=logger, gpus=0)
 
     train(trainer, div2k, model)
     test(trainer, div2k, model)
     
-    '''
-    wand.finish()
-    '''
+    if (use_wandb):
+        wandb.finish()
+        
+        # Delete local wandb files
+        print(os.path.abspath(os.getcwd()))
+        shutil.rmtree('wandb')
+        
