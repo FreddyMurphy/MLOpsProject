e5bba62770565385c2823f232c5c39f835ec590d
Patrick Nielsen <palleoel@gmail.com>
2021-06-16 14:46:28 +0200

    Update run_config.py

--- run_config.py
+++ run_config.py
@@ -8,13 +8,12 @@ print("Using azureml-core version", azureml.core.VERSION)
 ws = Workspace.from_config()
 
 # Create environment
-env = Environment(workspace=ws, name="pytorch_env")
+env = Environment.from_pip_requirements(name="myenv", file_path='requirements.txt')
 
 # Set dependencies of env (could also have used a dependencies.txt file instead)
 packages = CondaDependencies.create(conda_packages=['pip', 'pytorch','python==3.7', 'joblib'],
                                     pip_packages=['azureml-defaults', 'torchvision==0.7.0',
                                                   'pytorch_lightning', 'wandb'])
-packages.merge_requirements('requirements.txt')
 env.python.conda_dependencies = packages
 
 # I have created both a CPU and GPU compute targets
