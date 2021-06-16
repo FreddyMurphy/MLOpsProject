ce9af3f0e1182bf384d72c2bc5e47a5f019104a2
philipwastakenwastaken <s174274@student.dtu.dk>
2021-06-17 01:07:05 +0200

    Final version

--- test_environment.py
+++ test_environment.py
@@ -5,18 +5,22 @@ REQUIRED_PYTHON = "python3"
 
 def main():
     system_major = sys.version_info.major
+    system_minor = sys.version_info.minor
     if REQUIRED_PYTHON == "python":
         required_major = 2
     elif REQUIRED_PYTHON == "python3":
         required_major = 3
+        min_minor = 6
+        max_major = 9
     else:
-        raise ValueError("Unrecognized python interpreter: {}".format(
-            REQUIRED_PYTHON))
+        raise ValueError(
+            "Unrecognized python interpreter: {}".format(REQUIRED_PYTHON))
 
-    if system_major != required_major:
+    if system_major != required_major or not (system_minor >= min_minor
+                                              and system_minor < max_major):
         raise TypeError(
-            "This project requires Python {}. Found: Python {}".format(
-                required_major, sys.version))
+            "This project requires Python >= 3.6, < 3.9. Found: Python {}".
+            format(sys.version))
     else:
         print(">>> Development environment passes all tests!")
 
